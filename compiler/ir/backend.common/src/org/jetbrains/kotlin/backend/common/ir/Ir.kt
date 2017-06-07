157289df5d6c4c47e367cd0f3ab0c68b1b95b189
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2017-06-07 16:29:49 +0200

    ~callable

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/ir/Ir.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/ir/Ir.kt
@@ -123,7 +123,7 @@ open class Symbols<out T: CommonBackendContext>(val context: T, private val symb
 //
 //    val getContinuation = symbolTable.referenceSimpleFunction(
 //            context.getInternalFunctions("getContinuation").single())
-//
+
 //    val coroutineImpl = symbolTable.referenceClass(context.getInternalClass("CoroutineImpl"))
 
 //    val coroutineSuspendedGetter = symbolTable.referenceSimpleFunction(
@@ -132,7 +132,9 @@ open class Symbols<out T: CommonBackendContext>(val context: T, private val symb
 //                    .single().getter!!
 //    )
 
-    //val kFunctionImpl = symbolTable.referenceClass(context.reflectionTypes.kFgetInternalClass("KFunctionImpl"))
+    val reflectionTypes = context.reflectionTypes
+    val descriptor = reflectionTypes.kFunctionImpl
+    val kFunctionImpl = symbolTable.referenceClass(descriptor)
 
 //    val kProperty0Impl = symbolTable.referenceClass(context.reflectionTypes.kProperty0Impl)
 //    val kProperty1Impl = symbolTable.referenceClass(context.reflectionTypes.kProperty1Impl)
